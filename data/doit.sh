94156db1b2112a890bd2ee646cb45cf8ab5029cf
John Martinsson <john.martinsson@gmail.com>
2019-09-19 10:07:39 +0200

    Fix typo.

--- data/doit.sh
+++ data/doit.sh
@@ -1,7 +1,7 @@
-#python3 get_drive_file.py 0B7EVK8r0v71pZjFTYXZWM3FlRnM imgs_aligned.zip
-#python3 get_drive_file.py 0B7EVK8r0v71pblRyaVFSWGxPY0U annotations.txt
-#python3 get_drive_file.py 0B7EVK8r0v71pY0NSMzRuSXJEVkk data_split.txt
-#unzip imgs_aligned.zip
+python3 get_drive_file.py 0B7EVK8r0v71pZjFTYXZWM3FlRnM imgs_aligned.zip
+python3 get_drive_file.py 0B7EVK8r0v71pblRyaVFSWGxPY0U annotations.txt
+python3 get_drive_file.py 0B7EVK8r0v71pY0NSMzRuSXJEVkk data_split.txt
+unzip imgs_aligned.zip
 rm imgs_aligned.zip
 mv img_align_celeba imgs
-pyrhon3 preprocess_annotations.py
+python3 preprocess_annotations.py
